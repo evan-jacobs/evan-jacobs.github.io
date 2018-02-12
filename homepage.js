58246fe762624e59a83cffb4fab83f337b563ad7
Evan Jacobs <Evan@evans-mbp.home>
2018-02-11 23:55:30 -0800

    define app components

--- homepage.js
+++ homepage.js
@@ -1,1 +1,29 @@
 import React, { Component } from 'react';
+import { Text, View } from 'react-native';
+
+class App extends Component;
+
+import React, { Component } from 'react';
+import { Text, View } from 'react-native';
+
+class Header extends Component;
+
+import React, { Component } from 'react';
+import { Text, View } from 'react-native';
+
+class Projects extends Component;
+
+import React, { Component } from 'react';
+import { Text, View } from 'react-native';
+
+class Button extends Component;
+
+import React, { Component } from 'react';
+import { Text, View } from 'react-native';
+
+class List extends Component;
+
+import React, { Component } from 'react';
+import { Text, View } from 'react-native';
+
+class Row extends Component;
